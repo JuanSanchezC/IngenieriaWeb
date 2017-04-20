0361bc995f5814885cce5c28495901495b06398a
juansanchezc <juan.sanchezc@udea.edu.co>
2017-04-20 13:46:46 -0500

    Creacion de proyecto web con jersey

---
+++ JerseyClase/src/co/edu/udea/iw/ws/PrimerEjemplo.java
@@ -0,0 +1,44 @@
+package co.edu.udea.iw.ws;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/*
+ * @author juan.sanchezc@udea.edu.co
+ */
+
+@Path("saludo") // se define la URL con la que responderá el servicio
+public class PrimerEjemplo {
+	
+	@GET //se define que es un meto get
+	@Produces(MediaType.TEXT_HTML) //se define el tipo de dato que se retorna	
+	public String saludar(){
+		return "Buenas las tengan";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("2")
+	public String saludar2(){
+		return "<h1>Nuevo puto saludo</h1>";
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("3")
+	public String saludar3(@QueryParam("a")String nombre){
+		return "Hola puto "+nombre;
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Path("4/{a}")
+	public String saludar4(@PathParam("a") String nombre){
+		return "Hola puto "+nombre;
+	}
+}
